e9acc2f5882f1953d888743f15341a1848777a6c
eliran0220 <eliran0220@gmail.com>
2018-12-13 20:01:01 +0200

    lexer

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <list>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    cout << "Print natan defekt";
+
     return 0;
 }
+
+
+list<vector<string>> lexer(const string &fileName) {
+        ifstream file(fileName);
+        string line;
+        vector<string> command;
+        if (file.fail()) {
+            cout << "File doesn't exist";
+        } else {
+            while (!file.eof()) {
+                getline(file,line);
+            }
+
+        }
+
+}
+
+vector<string> splitCommand(const string &givenLine){
+    vector<string> vec;
+    stringstream ss(givenLine);
+    string item;
+    string delim = " ";
+    while (getline(ss, item, delim)){
+        vec.push_back(item);
+    }
+}
+
+
+
+
