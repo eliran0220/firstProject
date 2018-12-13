d32a41789d16b6df98c98ca479069bc1cfc88124
AfikAharon <afikaharon@gmail.com>
2018-12-13 19:43:39 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include "Expressions/Mult.h"
+#include "Expressions/Number.h"
+#include "Expressions/Minus.h"
+#include "Expressions/Plus.h"
+#include "Expressions/Div.h"
+
 using namespace std;
 int main() {
 
