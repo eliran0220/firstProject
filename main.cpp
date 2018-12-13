0e2724371919417e092dd7ec4414a544637a382f
AfikAharon <afikaharon@gmail.com>
2018-12-13 18:05:26 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    cout << "Print natan defekt";
-    cout<<"a שתוק יהומו";
+
     return 0;
 }
