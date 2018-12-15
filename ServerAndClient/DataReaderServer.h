cdbc910e55af408f14f586d3ff58ae9714cc4ca8
AfikAharon <afikaharon@gmail.com>
2018-12-15 22:26:58 +0200

    test

---
+++ ServerAndClient/DataReaderServer.h
@@ -0,0 +1,14 @@
+//
+// Created by afik on 12/15/18.
+//
+
+#ifndef FIRSTPROJECT_DATAREADERSERVER_H
+#define FIRSTPROJECT_DATAREADERSERVER_H
+
+
+class DataReaderServer {
+    DataReaderServer(){};
+};
+
+
+#endif //FIRSTPROJECT_DATAREADERSERVER_H
