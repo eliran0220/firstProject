ba26ff98582cc7de6bc598b5b9c4d9c60a3621be
AfikAharon <afikaharon@gmail.com>
2018-12-15 23:23:40 +0200

    test

---
+++ Expressions/CommandExpression.h
@@ -0,0 +1,25 @@
+//
+// Created by afik on 12/15/18.
+//
+
+#ifndef FIRSTPROJECT_COMMANDEXPRESSION_H
+#define FIRSTPROJECT_COMMANDEXPRESSION_H
+
+#include "Expression.h"
+#include "../Commands/Command.h"
+
+class CommandExpression : public Expression {
+
+private:
+    Command* command;
+    vector<string>* parameters;
+    int position;
+
+
+public:
+    virtual double calculate(){
+        return command->execute(*this->parameters, position);
+    }
+};
+
+#endif //FIRSTPROJECT_COMMANDEXPRESSION_H
