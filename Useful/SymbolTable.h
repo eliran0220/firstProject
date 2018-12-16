8ed0232d24df03562b06c0d85a3607427fbf26e3
eliran0220 <eliran0220@gmail.com>
2018-12-16 22:49:21 +0200

    fixed

--- Useful/SymbolTable.h
+++ Useful/SymbolTable.h
@@ -9,17 +9,17 @@
 
 using namespace std;
 
-template<typename T, typename K>
 class SymbolTable {
 private:
     map<string, double> symbolTable;
     map<string, string> destTable;
 
-    void add(T name, K value, char table);
-
-    void update(T name, K value, char table);
-
-    K getSymbtolValue(T name, char table);
+    void addToSymbol(string name, double value);
+    void addToDestTable(string name, string value);
+    void updateSymbtolTable(string name, double value);
+    void updateDestTable(string name, string value);
+    double getValueSymbtolTable(string name);
+    string getValueDestTable(string name);
 
 
 };
